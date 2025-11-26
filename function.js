444e279a0eb26f5cc32344bebbaf3985db712bc2
asm-ayesha <ayesha219333@gmail.com>
2025-11-26 15:35:49 +0600

    function and function syntax

---
+++ function.js
@@ -0,0 +1,10 @@
+let age = 23;
+
+// fucntion stracture
+function fanOffkor(){
+    console.log('bosa the uthe dara');
+    console.log('walk towards the switch')
+}
+
+//call the function
+fanOffkor()
