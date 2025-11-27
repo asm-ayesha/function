f07195864c8c570a3cb3c9289b905c6f4e551cc0
asm-ayesha <ayesha219333@gmail.com>
2025-11-27 10:19:40 +0600

    function works and argument vs parameter

---
+++ argument.js
@@ -0,0 +1,21 @@
+function doublIt(num){
+    let doubled = num * 2;
+    console.log(num, doubled);
+}
+
+console.log('I will call the function');
+doublIt(15);
+console.log('---------------------');
+doublIt(70);
+console.log('-----------------------');
+doublIt(531);
+console.log('---------------------------');
+doublIt(20);
+
+
+function difference(num1,num2){
+    let result = num1 -num2;
+    console.log(num1, num2, 'num1 num2 difference is ', result);
+}
+
+difference(50,10);
