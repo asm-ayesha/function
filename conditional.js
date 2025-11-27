f6c5c310c9aad2ea6e0ece38df49be7e351ed61b
asm-ayesha <ayesha219333@gmail.com>
2025-11-27 14:56:15 +0600

    conditional return of odd and even

---
+++ conditional.js
@@ -0,0 +1,18 @@
+function isEven(number){
+    if(number % 2 === 0){
+        return true;
+    }
+    return false;
+}
+
+console.log(isEven(31))
+
+
+function isOdd(number){
+    if(number % 2 !== 0){
+        return true;
+    }
+    return false;
+}
+
+console.log(isOdd(31))
---
+++ return.js
@@ -0,0 +1,7 @@
+function tenTimes(number){
+    let result = number *10;
+    return result;
+}
+
+let output =tenTimes(20);
+console.log(output)
